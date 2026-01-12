5d23a9b50e2a17ffd44012815ff2a5096c94d57c
xtr0py <xtr0py@protonmail.com>
2026-01-12 13:48:43 -0500

    Cleaning up citrix related code. Using VM workkaround for now.

--- configuration.nix
+++ configuration.nix
@@ -19,12 +19,12 @@
     curl git wget
   ];
   # Citrix needs to be marked as broken in 25.11? 
-  nixpkgs.config.allowBroken = true;
+  # nixpkgs.config.allowBroken = true;
 
   # Add insecure packages to be removed later
-   nixpkgs.config.permittedInsecurePackages = [
-                "mbedtls-2.28.10" "libsoup-2.74.3"
-              ];
+  # nixpkgs.config.permittedInsecurePackages = [
+  #              "mbedtls-2.28.10" "libsoup-2.74.3"
+  #            ];
   # Enable Citrix Module consider removing since citrix is not a system level
   # services.citrix.enable = true;
 
